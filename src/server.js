0897665f3d8b500894545afa809971af0fab63f2
KotDenisV <kot.denis@gmail.com>
2024-09-10 13:40:13 +0300

    MongoDB

--- src/server.js
+++ src/server.js
@@ -1,13 +1,32 @@
 import express from "express";
+import pino from 'pino-http';
+import cors from 'cors';
+import dotenv from "dotenv";
+import { env } from "./utils/env.js";
 
-
-
+dotenv.config();
+const PORT = Number(env('PORT', '3000'));
 
 export function setupServer() {
     
-    const app = express();
-
-    const PORT = 3000;
+    const app = express();    
+  
+    app.use(express.json());
+    app.use(cors());
+  
+    app.use(
+      pino({
+        transport: {
+          target: 'pino-pretty',
+        },
+      }),
+    );
+    
+    app.use('*', (req, res, next) => {
+      res.status(404).json({
+         message: 'Not found',
+      });
+    });
     
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
