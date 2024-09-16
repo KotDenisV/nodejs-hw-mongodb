a7ef8d4e2e1283cc03e2a28fc2e9a7750db48f0d
KotDenisV <kot.denis@gmail.com>
2024-09-16 22:25:00 +0300

    router

--- src/server.js
+++ src/server.js
@@ -3,7 +3,8 @@ import pino from 'pino-http';
 import cors from 'cors';
 import dotenv from "dotenv";
 import { env } from "./utils/env.js";
-import { getAllContacts, getContactById } from "./services/contacts.js";
+// import { getAllContacts, getContactById } from "./services/contacts.js";
+import contactsRouter from './routers/contacts.js';
 
 dotenv.config();
 const PORT = Number(env('PORT', '3000'));
@@ -23,33 +24,35 @@ export function setupServer() {
       }),
     );    
      
-    app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContacts();
+    // app.get('/contacts', async (req, res) => {
+    // const contacts = await getAllContacts();
 
-      res.status(200).json({
-        status: 200,
-	      message: "Successfully found contacts!",
-        data: contacts,
-      });
-    });
+    //   res.status(200).json({
+    //     status: 200,
+	  //     message: "Successfully found contacts!",
+    //     data: contacts,
+    //   });
+    // });
   
-    app.get('/contacts/:contactId', async (req, res, next) => {
-      const { contactId } = req.params;
-      const contact = await getContactById(contactId);    
+    // app.get('/contacts/:contactId', async (req, res, next) => {
+    //   const { contactId } = req.params;
+    //   const contact = await getContactById(contactId);    
     
-	    if (!contact) {
-	      res.status(404).json({
-		      message: 'Contact not found',
-	      });
-	      return;
-	    }
+	  //   if (!contact) {
+	  //     res.status(404).json({
+		//       message: 'Contact not found',
+	  //     });
+	  //     return;
+	  //   }
 	
-      res.status(200).json({
-        status: 200,
-	      message: `Successfully found contact with id ${contactId}!`,
-        data: contact,
-      });
-    });
+    //   res.status(200).json({
+    //     status: 200,
+	  //     message: `Successfully found contact with id ${contactId}!`,
+    //     data: contact,
+    //   });
+    // });
+    
+  app.use(contactsRouter);
   
     app.use('*', (req, res, next) => {
       res.status(404).json({
