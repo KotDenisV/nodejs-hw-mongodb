539b78bce46e30ba084dbbedea78e02212cc0296
KotDenisV <kot.denis@gmail.com>
2024-09-17 13:31:06 +0300

    Patch/delete

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,4 +1,7 @@
-import { getAllContacts, getContactById, createContact } from "../services/contacts.js";
+import {
+    getAllContacts, getContactById,
+    createContact, updateContact, deleteContact
+} from "../services/contacts.js";
 import createHttpError from "http-errors";
 
 export const getContactsController = async (req, res) => {
@@ -34,4 +37,31 @@ export const createContactController = async (req, res) => {
         message: "Successfully created a contact!",
         data: contact,
     });
+};
+
+export const patchContactController = async (req, res, next) => {
+  const { contactId } = req.params;
+  const result = await updateContact(contactId, req.body);
+
+  if (!result) {
+    next(createHttpError(404, 'Contact not found'));
+    return;
+  }
+
+  res.json({
+    status: 200,
+    message: 'Successfully patched a contact!',
+    data: result.contact,
+  });
+};
+
+export const deleteContactController = async (req, res, next) => {
+    const { contactId } = req.params;
+    const contact = await deleteContact(contactId);
+
+    if (!contact) {
+        next(createHttpError(404, 'Contact not found'));
+    }
+
+    res.status(204).send();
 };
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,6 +1,10 @@
 import { Router } from "express";
 // import { getAllContacts, getContactById } from "../services/contacts.js";
-import { getContactsController, getContactByIdController, createContactController } from "../controllers/contacts.js";
+import {
+    getContactsController, getContactByIdController,
+    createContactController, patchContactController,
+    deleteContactController
+} from "../controllers/contacts.js";
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
 
 const router = Router();
@@ -10,5 +14,9 @@ router.get('/contacts', ctrlWrapper(getContactsController));
 router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
 
 router.post('/contacts', ctrlWrapper(createContactController));
+
+router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
+
+router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
 
 export default router;
