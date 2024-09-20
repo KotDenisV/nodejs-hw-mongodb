def231b18c5cce3468ff8d5f23ee3d412161d63d
KotDenisV <kot.denis@gmail.com>
2024-09-20 12:01:42 +0300

    final update

--- src/server.js
+++ src/server.js
@@ -3,7 +3,6 @@ import pino from 'pino-http';
 import cors from 'cors';
 import dotenv from "dotenv";
 import { env } from "./utils/env.js";
-// import { getAllContacts, getContactById } from "./services/contacts.js";
 import contactsRouter from './routers/contacts.js';
 import { errorHandler } from "./middlewares/errorHandler.js";
 import { notFoundHandler } from "./middlewares/notFoundHandler.js";
@@ -26,41 +25,7 @@ export function setupServer() {
       }),
     );    
      
-    // app.get('/contacts', async (req, res) => {
-    // const contacts = await getAllContacts();
-
-    //   res.status(200).json({
-    //     status: 200,
-	  //     message: "Successfully found contacts!",
-    //     data: contacts,
-    //   });
-    // });
-  
-    // app.get('/contacts/:contactId', async (req, res, next) => {
-    //   const { contactId } = req.params;
-    //   const contact = await getContactById(contactId);    
-    
-	  //   if (!contact) {
-	  //     res.status(404).json({
-		//       message: 'Contact not found',
-	  //     });
-	  //     return;
-	  //   }
-	
-    //   res.status(200).json({
-    //     status: 200,
-	  //     message: `Successfully found contact with id ${contactId}!`,
-    //     data: contact,
-    //   });
-    // });
-    
     app.use(contactsRouter);
-  
-    // app.use('*', (req, res, next) => {
-    //   res.status(404).json({
-    //      message: 'Not found',
-    //   });
-  // });
   
     app.use('*', notFoundHandler);
 
