066ed9eb38fd477817ffcd89a58e7783a11eb4a0
KotDenisV <kot.denis@gmail.com>
2024-10-02 22:02:52 +0300

    logout

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -4,12 +4,15 @@ import { registerUserSchema } from "../validation/auth.js";
 import { registerUserController } from "../controllers/auth.js";
 import { validateBody } from "../middlewares/validateBody.js";
 import { loginUserSchema } from "../validation/auth.js";
-import { loginUserController } from "../controllers/auth.js";
+import { loginUserController, logoutUserController } from "../controllers/auth.js";
+
 
 const router = Router();
 
 router.post('/register', validateBody(registerUserSchema), ctrlWrapper(registerUserController));
 
 router.post('/login', validateBody(loginUserSchema), ctrlWrapper(loginUserController));
+
+router.post('/logout', ctrlWrapper(logoutUserController));
 
 export default router;
